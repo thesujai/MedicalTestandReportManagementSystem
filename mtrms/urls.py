a84aa10c9ad784b6d1cf83e88ce7bc800942653a
thesujai <b210031@nitsikkim.ac.in>
2023-08-20 00:43:43 +0530

    added views and routed login page

--- mtrms/urls.py
+++ mtrms/urls.py
@@ -2,4 +2,6 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('',views.index,name='index'),
+    path('login/patient',views.login_patient,name='login_patient'),
+    path('login/doctor',views.login_doctor,name='login_doctor'),
 ]
--- mtrms/views.py
+++ mtrms/views.py
@@ -4,3 +4,15 @@ from django.shortcuts import render,HttpResponse
 def index(request):
     # return HttpResponse("Hello i am shaurya")
     return render(request,'mtrms/index.html')
+
+def login_patient(request):
+    return render(request,'mtrms/login.html')
+
+def login_doctor(request):
+    return render(request,'mtrms/login.html')
+
+def signup_patient(request):
+    return render(request,'mtrms/signup.html')
+
+def signup_doctor(request):
+    return render(request,'mtrms/signup.html')
