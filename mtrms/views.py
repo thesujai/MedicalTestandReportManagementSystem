f266a494d96d5986596ec80e107c9a89784e26a7
thesujai <b210031@nitsikkim.ac.in>
2023-08-20 00:18:02 +0530

    made the homepage

--- mtrms/views.py
+++ mtrms/views.py
@@ -2,4 +2,5 @@ from django.shortcuts import render,HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello i am shaurya")
+    # return HttpResponse("Hello i am shaurya")
+    return render(request,'mtrms/index.html')
