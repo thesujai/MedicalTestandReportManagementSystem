fecfda659a37722249c7c5d001b4d80c0ad7a544
thesujai <b210031@nitsikkim.ac.in>
2023-08-20 00:45:20 +0530

    routed urls for signup page

--- mtrms/urls.py
+++ mtrms/urls.py
@@ -4,4 +4,7 @@ urlpatterns = [
     path('',views.index,name='index'),
     path('login/patient',views.login_patient,name='login_patient'),
     path('login/doctor',views.login_doctor,name='login_doctor'),
+    path('signup/patient',views.signup_patient,name='signup_patient'),
+    path('signup/doctor',views.signup_doctor,name='signup_doctor'),
+    
 ]
