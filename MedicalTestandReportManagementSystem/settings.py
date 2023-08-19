1e525f6fbf8ec6a670db9d95d21670072db18585
thesujai <b210031@nitsikkim.ac.in>
2023-08-19 16:48:55 +0530

    app registered

--- MedicalTestandReportManagementSystem/settings.py
+++ MedicalTestandReportManagementSystem/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'mtrms.apps.MtrmsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
