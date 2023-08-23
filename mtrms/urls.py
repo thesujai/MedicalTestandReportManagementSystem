f1fe376a6bba4af7f36c2ec85cd16de407b1a8bd
thesujai <b210031@nitsikkim.ac.in>
2023-08-23 23:56:48 +0530

    set url for login page for doctor

--- mtrms/urls.py
+++ mtrms/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from . import views
+from .views import LoginDoctor,login_patient,signup_doctor,signup_patient
 app_name='mtrms'
 urlpatterns = [
     path('',views.index,name='index'),
     path('login/patient',views.login_patient,name='login_patient'),
-    path('login/doctor',views.login_doctor,name='login_doctor'),
+    path('login/doctor',LoginDoctor.as_view(),name='login_doctor'),
     path('signup/patient',views.signup_patient,name='signup_patient'),
     path('signup/doctor',views.signup_doctor,name='signup_doctor'),
     
