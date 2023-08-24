beb58b8f91a7a743b6f9979bcf8e9427a8eb35d8
thesujai <b210031@nitsikkim.ac.in>
2023-08-24 12:17:59 +0530

    removed User  registeration

--- mtrms/admin.py
+++ mtrms/admin.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
-from .models import User,Doctor,Patient,Test,Appointment,Report
+from .models import Doctor,Patient,Test,Appointment,Report
 
 # Register your models here.
-admin.site.register(User)
 admin.site.register(Doctor)
 admin.site.register(Patient)
 admin.site.register(Appointment)
 admin.site.register(Test)
 admin.site.register(Report)
+
