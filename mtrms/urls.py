dc05d6f24b4b9efea3bef1b8a6c019a15c0464b1
thesujai <b210031@nitsikkim.ac.in>
2023-08-26 01:26:39 +0530

    url for user creation

--- mtrms/urls.py
+++ mtrms/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
 from . import views
-from .views import LoginDoctor,ChangePassword, LoginPatient
+from .views import LoginDoctor,ChangePassword, LoginPatient, PatientSignup
 app_name='mtrms'
 urlpatterns = [
     path('',views.index,name='index'),
     path('login/patient',LoginPatient.as_view(),name='login_patient'),
     path('login/doctor',LoginDoctor.as_view(),name='login_doctor'),
-    path('signup/patient',views.signup_patient,name='signup_patient'),
+    path('signup/patient',PatientSignup.as_view(),name='signup_patient'),
     path('signup/doctor',views.signup_doctor,name='signup_doctor'),
     path('doctor-dashboard/', views.doctor_dashboard, name='doctor-dashboard'),
     path('patient-dashboard/', views.patient_dashboard, name='patient-dashboard'),
