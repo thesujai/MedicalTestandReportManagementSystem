4ee068d7f0c2e3f1c9a53a91e51a994375bcb9bc
thesujai <b210031@nitsikkim.ac.in>
2023-08-25 16:06:05 +0530

    updated url for patient login

--- mtrms/urls.py
+++ mtrms/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 from . import views
-from .views import LoginDoctor,ChangePassword
+from .views import LoginDoctor,ChangePassword, LoginPatient
 app_name='mtrms'
 urlpatterns = [
     path('',views.index,name='index'),
-    path('login/patient',views.login_patient,name='login_patient'),
+    path('login/patient',LoginPatient.as_view(),name='login_patient'),
     path('login/doctor',LoginDoctor.as_view(),name='login_doctor'),
     path('signup/patient',views.signup_patient,name='signup_patient'),
     path('signup/doctor',views.signup_doctor,name='signup_doctor'),
